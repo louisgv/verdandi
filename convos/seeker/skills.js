d09cff11638138d7e9f550b1f382a69440f8d2ce
Louis Vichy <lgvichy@gmail.com>
2016-04-15 01:56:38 -0700

    Refactored Skill code

--- convos/seeker/skills.js
+++ convos/seeker/skills.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 let Skills = {};
@@ -9,18 +8,44 @@ const Next = require('./experiences');
 
 const Utils = require('../utils');
 
+function askSkillItem(r, c, m, d, b) {
+	c.ask(Utils.response('What\'re your skills?', 'skills'),
+		function (r, c) {
+			c.say(Utils.response('You said : ' + r.text));
+
+			Next.ask(r, c, m, d, b);
+
+			c.next();
+		});
+}
+
+function askSkillCategory(r, c, m, d, b) {
+	c.ask(Utils.response('What\'re your skills?', 'skills'),
+		function (r, c) {
+			c.say(Utils.response('You said : ' + r.text));
+
+			askSkillItem(r, c, m, d, b);
+
+			c.next();
+		});
+}
+
 Skills.ask = function (r, c, m, d, b) {
-  if(m.skills) {
-    c.ask(Utils.response('What\'re your skills?', 'skills'),
-      function (r, c) {
-        c.say(Utils.response('You said : ' + r.text));
+	if(m.skills) {
+		c.say(Utils.response('I think you forgot to list your skill set. :thinking_face:', 'skill'));
+		Consent.ask(r, c, b, function (allowed) {
+			if(allowed) {
 
-        Next.ask(r, c, m, d, b);
+				askSkillCategory(r, c, m, d, b);
 
-        c.next();
-      });
-  } else {
-    Next.ask(r, c, m, d, b);
-    c.next();
-  }
+			} else {
+				Next.ask(r, c, m, d, b);
+
+				c.next();
+			}
+		})
+	} else {
+		Next.ask(r, c, m, d, b);
+		c.next();
+	}
 }
